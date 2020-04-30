dd68f07693de280ecf6c9598c68974c7dafc915d
NemoHoHaloAi <496791354@qq.com>
2020-04-30 09:42:19 +0800

    完成打开 - 快乐数

---
+++ LeetCode/202_happy_number.py
@@ -0,0 +1,42 @@
+# 202. 快乐数
+# 编写一个算法来判断一个数 n 是不是快乐数。
+# 
+# 「快乐数」定义为：对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和，然后重复这个过程直到这个数变为 1，
+#  也可能是 无限循环 但始终变不到 1。如果 可以变为  1，那么这个数就是快乐数。
+# 
+# 如果 n 是快乐数就返回 True ；不是，则返回 False 。
+# 
+#  
+# 
+# 示例：
+# 
+# 输入：19
+# 输出：true
+# 解释：
+# 1^2 + 9^2 = 82
+# 8^2 + 2^2 = 68
+# 6^2 + 8^2 = 100
+# 1^2 + 0^2 + 02 = 1
+
+class Solution:
+    def isHappy(self, n):
+        '''
+        记录变换的过程，如果出现一样的说明循环回来了，就不需要再判断了
+        '''
+        steps = []
+        while True:
+            step = [((int(n/(10**i)))%10)**2 for i in range(len(str(n)))]
+            if sum(step)==1:
+                return True
+            step = sorted(step)
+            if steps.count(step) > 0:
+                return False
+            steps.append(step)
+            n = sum(step)
+        return False
+
+case = 19
+print(Solution().isHappy(case))
+
+case = 2
+print(Solution().isHappy(case))
